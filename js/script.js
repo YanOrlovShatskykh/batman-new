c7ea7d56903b73c4b27ad3a40f592cc3afcf2c24
Yan OrlovShatskykh <yan.orlov.shatskykh@gmail.com>
2020-04-01 19:51:50 +0300

    Add project on GitHub

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  // for baners
+  const banner = document.querySelector('div');
+  const bottomBanner = document.querySelectorAll('.cbalink');
+
+  if(!banner.classList.contains('container')) {
+    banner.style.display = 'none';
+  }
+
+  bottomBanner.forEach(elem => {
+    elem.style.display = 'none';
+  });
+
+
+  
+  let tabsItem = $('.tabs-item');
+
+  tabsItem.on('click', function(event) {
+    event.preventDefault();
+    let activeContent = $(this).attr('href');
+    $('.visible').toggleClass('visible');
+    $(activeContent).toggleClass('visible');
+    $('.tabs-item-active').toggleClass('tabs-item-active');
+    $(this).toggleClass('tabs-item-active');
+  });
+});
